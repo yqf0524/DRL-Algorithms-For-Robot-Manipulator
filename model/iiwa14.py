cd2f09bf14b058b9f5169b5c212765040074d00b
yqf0524 <yangqingfeng0524@gmail.com>
2021-02-12 01:39:26 +0100

    delete klampt.math.so3 change to scipy (Rotation)

--- model/iiwa14.py
+++ model/iiwa14.py
@@ -1,7 +1,7 @@
 from klampt.model import collide
-from klampt import math
 from klampt import WorldModel
 from klampt import vis
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 import sympy
 
@@ -20,8 +20,10 @@ class Model:
         """
 
     def __init__(self):
-        self.position_limit = np.rad2deg(np.array([168, 118, 168, 118, 168, 118, 173]))
-        self.velocity_limit = np.rad2deg(np.array([85, 85, 100, 75, 130, 135, 135]))
+        self.joint_limit = np.rad2deg(np.array([168, 118, 168, 118, 168, 118, 173]))
+        self.velocity_scale = 0.1
+        self.velocity_limit = self.velocity_scale * \
+                              np.rad2deg(np.array([85, 85, 100, 75, 130, 135, 135]))
         self.link_length = np.array([0.36, 0.42, 0.4, 0.126], dtype=np.float32)
         self.DH_params = np.array([
             [0.0, 0.36, 0.0, -np.pi / 2],
@@ -33,27 +35,37 @@ class Model:
             [0.0, 0.126, 0.0, 0.0]], dtype=np.float32)
         self.link_mass = np.array([])
         self.world = WorldModel()
-        self.world.loadElement('./iiwa14.urdf')
+        self.world.loadElement('model/iiwa14.urdf')
         self.robot = self.world.robot(0)
         vis.add("iiwa", self.robot)
-        self.init_configuration = np.zeros(7)
 
-    def so3_to_rpy(self, so3_orientation):
+    def so3_to_rpy(self, so3):
         """
-              [a11,a12,a13]
-        so3 = [a21,a22,a23]
-              [a31,a32,a33]
-              
-        Klamp’t represents the matrix as a list
-        [a11, a21, a31, a12, a22, a32, a13, a23, a33].
+        Converts an SO3 rotation matrix to rpy angles
 
-        :param so3_orientation: orientation of end effector
-        :return: rpy corresponse to (X, Y, Z)
+        :param so3: rotation matrix
+        :return: list of rpy angles
         """
-        so3 = np.reshape(so3_orientation, (1, -1), order="F").squeeze(axis=0)
-        so3 = list(so3)
-        rpy = math.so3.rpy(so3)
+        r = R.from_matrix(so3)
+        rpy = r.as_euler("xyz")
         return rpy
+
+    def check_joint_limit(self, configuration):
+        in_joint_limit = (np.abs(configuration) <= self.joint_limit).all()
+        if not in_joint_limit:
+            return False, np.clip(configuration, -self.joint_limit, self.joint_limit)
+        return True, configuration
+
+    def clip_velocity(self, action):
+        """
+        Set collection frequency of data to 100 HZ. Approximate
+        current action as current velocity
+
+        :param action: action will be taken based on current observation
+        :return: the clipped action
+        """
+        clip_action = np.clip(action, -self.velocity_limit, self.velocity_limit)
+        return clip_action
 
     def check_collision(self, configuration):
         """
@@ -88,9 +100,7 @@ class KinematicModel(Model):
 
     def __init__(self):
         super(KinematicModel, self).__init__()
-        self.current_configuration = self.init_configuration
-        self.current_ee_se3 = None
-        self.current_ee_so3 = None
+        self.current_configuration = np.deg2rad([-120, -90, 78, 62, -83, -100, 148])
         self.current_ee_position = None
         self.current_ee_rpy = None
         self.update_kinematic()
@@ -100,16 +110,17 @@ class KinematicModel(Model):
         Calculate the forward kinematic to get current pose of iiwa
 
         :param configuration: current joint position
-        :param link_length_noise: current link length disturbance (7, 1)
-        :return: current pose
+        :param link_length_noise: current link length disturbance (1, 7)
+        :return: current se3
         """
         ee_se3 = np.eye(4)
-        theta = configuration + self.DH_params[:, 1]
-        length = np.transpose(self.DH_params[:, 2]) + link_length_noise
-        offset = np.transpose(self.DH_params[:, 3])
-        alpha = np.transpose(self.DH_params[:, 4])
+        theta = configuration  # + np.transpose(self.DH_params[:, 0])
+        length = np.transpose(self.DH_params[:, 1]) + link_length_noise
+        offset = [0, 0, 0, 0, 0, 0, 0]  # np.transpose(self.DH_params[:, 2])
+        alpha = np.transpose(self.DH_params[:, 3])
         for i in range(len(theta)):
-            ee_se3 = np.matmul(ee_se3, self._homo_matrix(theta, length, offset, alpha))
+            ee_se3 = np.matmul(
+                ee_se3, self._homo_matrix(theta[i], length[i], offset[i], alpha[i]))
         return ee_se3
 
     def _homo_matrix(self, theta, length, offset, alpha):
@@ -124,19 +135,21 @@ class KinematicModel(Model):
 
         :return: homogeneous matrix
         """
-        Tr = np.array([
-            [np.cos(theta), -np.sin(theta) * np.cos(alpha), np.sin(theta) * np.sin(alpha), length * np.cos(theta)],
-            [np.sin(theta), np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), length * np.sin(theta)],
-            [0, np.sin(alpha), np.cos(alpha), offset],
-            [0, 0, 0, 1]], dtype=np.float32)
-        return Tr
+        tr = np.array([
+            [np.cos(theta), -np.sin(theta) * np.cos(alpha),
+             np.sin(theta) * np.sin(alpha), length * np.cos(theta)],
+            [np.sin(theta), np.cos(theta) * np.cos(alpha),
+             -np.cos(theta) * np.sin(alpha), length * np.sin(theta)],
+            [0.0, np.sin(alpha), np.cos(alpha), offset],
+            [0.0, 0.0, 0.0, 1.0]], dtype=np.float32)
+        return tr
 
     def update_kinematic(self):
-        self.current_ee_se3 = self.forward_kinematic(
-            self.current_configuration, self.link_gaussian_noise(0, 2, 8))
-        self.current_ee_so3 = self.current_ee_se3[0:3, 0:3]
-        self.current_ee_position = self.current_ee_se3[3, 0:3]
-        self.current_ee_rpy = self.so3_to_rpy(self.current_ee_so3)
+        current_ee_se3 = self.forward_kinematic(
+            self.current_configuration, self.link_gaussian_noise(0, 2, 7))
+        current_ee_so3 = current_ee_se3[0:3, 0:3]
+        self.current_ee_position = np.transpose(current_ee_se3[3, 0:3])
+        self.current_ee_rpy = self.so3_to_rpy(current_ee_so3)
 
     def link_gaussian_noise(self, mean, var, shape):
         return np.random.normal(mean, var, shape)
